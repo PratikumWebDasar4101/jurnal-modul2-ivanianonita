45503b7fd2d49ae0f7439a52b5fb2193105541a9
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2018-09-10 10:02:21 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,46 @@
-<?php 
+
+$angka=$_POST['angka'];
+
+echo "angka = $angka<br>";
+echo "Bentuk Piramida<br>";
+
+function rekrusif($angka){
+	global $i;
+	rekrusif2($i, $angka);
+	echo"<br>";
+	if (--$angka>$i){
+		rekrusif($angka);
+	}
+}
+function rekrusif2($j,$angka){
+	global $i;
+	if($j > $i - $angka){
+		echo"*";
+		rekrusif2($j-1, $angka);
+	}
+}
+
+$inputan = $_POST["input"];
+$i=0;
+rekrusif($inputan);
+
+for($i=1;$i<=$inputan; $i++){
+	if($i % 2 == 0){
+		$genap[]=$i;
+
+	}else{
+		$ganjil[]=$i;
+	}
+}
+ foreach ($genap as $bil_genap) {
+ 	echo $bil_genap.',';
+ 	# code...
+ }
+ echo"<br>";
+ foreach($ganjil as $bil_ganjil){
+ 	echo $bil_ganjil.',';
+ }
+ <?php 
 //Tuliskan kode anda di bawah ini
 
 ?>
